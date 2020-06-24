d0ac3aa9abeeee66b469cdbd51332f498e89c4ff
Biabiany <damienbiabiany@yahoo.fr>
2020-06-24 17:45:19 +0200

    complete media database

--- index.php
+++ index.php
@@ -49,3 +49,43 @@ else:
 
 endif;
 
+/*
+  
+  // CREATE ROUTER
+  // echo "selected media =".$_GET['media'].'<br>';
+  // echo "request url =". $_SERVER['REQUEST_URI'];
+
+  // idfilm ?media=
+  
+  $mediaId = $_GET['media'];
+
+  //echo "<br>\$_GET['media'] =".$_GET['media'];
+
+  $request = $_SERVER['REQUEST_URI'];
+  //echo "<br>request =".$request;
+
+
+  //echo "<br>".__DIR__ . '/index.php?media=2';
+
+  switch ($request) {
+      case '/CodFlix/index.php?media=2' :
+
+          // display the detail of the film
+          require __DIR__ . '/view/mediaDetailView.php';
+
+          break;
+      case '/' :
+          require __DIR__ . '/view/index.php';
+          break;
+      case '' :
+          require __DIR__ . '/view/index.php';
+          break;
+      case '/about' :
+          require __DIR__ . '/view/about.php';
+          break;
+      default:
+          http_response_code(404);
+          require __DIR__ . '/view/404.php';
+          break;
+  }
+ */
